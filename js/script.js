3a7771dca9ebf3df932435b94afc22c5b5cbd11c
Vsevolod Railean <sevarailean61@gmail.com>
2022-08-02 18:15:01 +0300

    practice pt4

--- js/script.js
+++ js/script.js
@@ -1,162 +1,75 @@
 "use strict";
 
-// const obj = {
-// 	a: 5,
-// 	b: 1 
-// };
+const personalMovieDB = {
+	count: 0,
+	movies:{},
+	actors:{},
+	geners:[],
+	privat: false,
+	start: function() {
+		personalMovieDB.count = +prompt("How many movies did you watch?", "");
 
-// const copy = obj; 
-
-// function copy(mainObject) {
-// 	let objCopy = {};
-
-// 	let key;
-// 	for (key in mainObject) {
-// 		objCopy[key] = mainObject[key];
-// 	}
-
-// 	return objCopy;
-// }
-
-// const numbers = {
-// 	a: 2,
-// 	b: 5,
-// 	c: {
-// 		x: 7,
-// 		y: 4
-// 	}
-// };
-
-// const newNumbers = copy(numbers);
-
-// newNumbers.a = 10;
-
-// console.log(newNumbers);
-// console.log(numbers);
-
-// const add = {
-// 	d: 17,
-// 	e: 20
-// };
-
-// const clone = Object.assign({}, add);
-
-
-// clone.d = 20;
-
-// console.log(add);
-// console.log(clone);
-
-// const oldArray = ['a', 'b', 'c'];
-// const newArray = oldArray.slice();
-
-// newArray[1] = 'Bum'
-
-// console.log(newArray);
-// console.log(oldArray);
-
-// const video = ['youtube', 'vimeo', 'rutube'],
-// 	  blogs = ['wordpress', 'livejournal', 'blogger'],
-// 	  internet = [...video, ...blogs, 'vk', 'facebook'];
-
-// console.log(internet);
-
-// function log(a, b, c) {
-// 	console.log(a);
-// 	console.log(b);
-// 	console.log(c);
-// }
-
-// const num = [2, 5, 7];
-
-// log(...num);
-
-// const arry = ["a", "b"];
-// const newArrray = [...arry];
-
-// const q = {
-// 	one: 1,
-// 	two: 2
-// };
-
-// const newQ = {...q};
-
-
-// const perosnalPlanPeter = {
-// 	name: "Peter",
-// 	age: "29",
-// 	skills: {
-// 		languages: ['ru', 'eng'],
-// 		programmingLangs: {
-// 			js: '20%',
-// 			php: '10%'
-// 		},
-// 		exp: '1 month'
-// 	},
-// 	 showAgeAndLangs: function(plan) {
-// 		let str = '';
-
-// 		const {age} = plan;
-// 		const {languages} = plan.skills;
-
-// 		str += `I'm ${age} and I know: `;
-
-// 		languages.forEach(function(language){
-// 			str += `${language.toUpperCase()} `;
-// 		});
-// 		return str;
-// 	 }
-// };
-
-// console.log(perosnalPlanPeter.showAgeAndLangs(perosnalPlanPeter)); 
-
-// function showExperience(plan) {
-// 	const {exp} = plan.skills;
-// 	return exp;
-// }
-
-// console.log(showExperience(perosnalPlanPeter)); 
-
-// function showProgrammingLangs(plan) {
-// 	let str = '';
-// 	const {programmingLangs} = plan.skills;
-
-// 	for (let key in programmingLangs) {
-// 		str += `Language ${key} is learned ${programmingLangs[key]};\n`;
-// 	}
-// 	return str;
-// }
-
-// console.log(showProgrammingLangs(perosnalPlanPeter));
-
-const family = ['Peter', 'Ann', 'Alex', 'Linda'];
-
-function showFamily(arr) {
-	let str = '';
-
-	if (arr.length !== 0) {
-		str = 'Family: ';
+	while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+		personalMovieDB.count = +prompt("How many movies did you watch?", "");
+	}
+	},
+	rememberMyFilms: function() {
+		for (let i = 0; i < 2; i++) {
+		const a = prompt("Last movie you watched", ""),
+			  b = prompt("Rate it from 1 to 10", "");
+	
+		if (a != " " && b != " " && a != null && b != null && a.length < 50) {
+			personalMovieDB.movies[a] = b;
+			console.log("done!");
+		} else {
+			console.log("wtf man");
+			i--;
+		}
+		
+	}
+	},
+	detectPersonalLevel: function() {
+		if (personalMovieDB.count < 10) {
+		console.log("Not a lot of movies watched");
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+		console.log("Clasical watcher");
+	} else if (personalMovieDB.count >= 30) {
+		console.log("Movie man");
 	} else {
-		str = 'No family(';
+		console.log("Error");
+	}
+	},
+	showMyDB: function() {
+		if (!personalMovieDB.privat) {
+		return personalMovieDB;
+	}
+	},
+	toggleVisibleMyDB: function() {
+		if (personalMovieDB.privat) {
+			personalMovieDB.privat = false;
+		} else {
+			personalMovieDB.privat = true;
+		}
+	},
+	writeYourGenres: function() {
+		for (let i = 1; i <= 3; i++) {
+			let genre = prompt(`Your favorite genre ${i}`,"").toLowerCase();
+			
+			if (genre === null || genre === "") {
+				i--;
+			} else {
+				personalMovieDB.geners[i - 1] = genre;
+			}
+		}
+		personalMovieDB.geners.forEach((item, i) => {
+			console.log(`Genre number ${i + 1} is ${item}`);
+		});
 	}
 
-	arr.forEach(member => {
-		str += `${member} `;
-	});
+};
 
-	return str;
-}
-
-console.log(showFamily(family));
-
-
-const favoriteCities = ['liSbon', 'ROME', 'miLan', 'Dublin'];
-
-function standardizeStrings(arr) {
-	arr.forEach(element => {
-		console.log(element.toLowerCase());
-	});
-
-}
-
-standardizeStrings(favoriteCities);
+console.log(personalMovieDB.start());
+console.log(personalMovieDB.rememberMyFilms());
+console.log(personalMovieDB.detectPersonalLevel());
+console.log(personalMovieDB.showMyDB());
+console.log(personalMovieDB.writeYourGenres());
