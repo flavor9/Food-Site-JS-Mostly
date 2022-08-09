4a23ba51aa5d5f9a39096bfc316ad3a6f6165a99
Vsevolod Railean <sevarailean61@gmail.com>
2022-08-09 19:08:12 +0300

    useful stuff p6

--- js/script.js
+++ js/script.js
@@ -1,138 +1,57 @@
 "use strict";
 
-// const shoppingMallData = {
-// 	shops: [
-// 		{
-// 			width: 10,
-// 			length: 5
-// 		},
-// 		{
-// 			width: 15,
-// 			length: 7
-// 		},
-// 		{
-// 			width: 20,
-// 			length: 5
-// 		},
-// 		{
-// 			width: 8,
-// 			length: 10
-// 		}
-// 	],
-// 	height: 5,
-// 	moneyPer1m3: 30,
-// 	budget: 50000
-// };
+const box = document.getElementById('box'),
+	  btns = document.getElementsByTagName('button'),
+	  circles = document.getElementsByClassName('circle'),
+	  hearts = document.querySelectorAll('.heart'),
+	  oneHeart = document.querySelector('.heart'),
+	  wrapper = document.querySelector('.wrapper');
 
-// function isBudgetEnough(data) {
-// 	let square = 0;
-// 	let volume = 0;
 
-// 	data.shops.forEach(shop => {
-// 		square += shop.width * shop.length;
-// 	});
+// box.style.backgroundColor =  'blue';
+// box.style.width = '500px';
 
-// 	volume = data.height * square;
+btns[1].style.borderRadius = '100%';
 
-// 	if (data.budget >= volume * data.moneyPer1m3 ) {
-// 		console.log("Enough budget");
-// 	} else {
-// 		console.log("Not enough budget");
-// 	}
+box.style.cssText = 'background-color: blue; width: 500px;';
 
-// 	console.log(square);
-// 	console.log(volume);
+circles[0].style.backgroundColor = 'red';
 
-	
+// for (let i = 0; i < hearts.length; i++) {
+// 	hearts[i].style.backgroundColor = 'blue';
 // }
 
-// isBudgetEnough(shoppingMallData);
+hearts.forEach(item => {
+	item.style.backgroundColor = 'orange';
+});
 
+const div = document.createElement('div');
 
-// const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam', 'Alex'];
+// const text = document.createTextNode('Me');
 
-// function sortStudentsByGroups(arr) {
-// 	arr.sort();
-// 	const a = [], b = [], c = [], rest = [];
+div.classList.add('black');
 
-// 	for (let i = 0; i < arr.length; i++) {
-// 		if (i < 3) {
-// 			a.push(arr[i]);
-// 		} else if (i < 6) {
-// 			b.push(arr[i]);
-// 		} else if (i < 9) {
-// 			c.push(arr[i]);
-// 		} else {
-// 			rest.push(arr[i]);
-// 		}
-// 	}
+wrapper.append(div);
+// wrapper.appendChild(div);
 
-// 	let result = [
-// 		a, b, c, `Left students: ${rest.length === 0 ? '-' : rest.join(', ')}`
-// 	];
-	
-// 	return result;
-// }
+// wrapper.prepend(div);
 
-// console.log(sortStudentsByGroups(students)); 
+// hearts[0].before(div);
 
+// hearts[0].after(div);
 
-const restorantData = {
-	menu: [
-		{
-			name: 'Salad Caesar',
-			price: '14$'
-		},
-		{
-			name: 'Pizza Diavola',
-			price: '9$',
-		},
-		{
-			name: 'Beefstake',
-			price: '17$'
-		},
-		{
-			name: 'Napoleon',
-			price: '7$'
-		}
-	],
-	waitors: [	
-		{name: 'Alice', age: 22}, {name: 'John', age: 24}
-	],
-	averageLunchPrice: '20$',
-	openNow: true
-};
+// wrapper.insertBefore(div, hearts[1]);
 
-function isOpen(prop) {
-	let answer = '';
-	prop ? answer = 'Open' : answer = 'Closed';
-
-	return answer;
-}
-
-console.log(isOpen(restorantData.openNow));
-
-
-function isAverageLunchPriceTrue(fDish, sDish, average) {
-	if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < (+average.slice(0, -1))) {
-		return 'Price lower than avergae';
-	} else {
-		return 'Price higher than avergae';
-	}
-}
-
-console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
-
-function tranfserWaitors(data) {
-	const copy = Object.assign({}, data);
-
-	copy.waitors = [{name: 'Mike', age: 32}];
-	return copy;
-}
+// circles[0].remove();
+// wrapper.removeChild(hearts[1]);
 
 
 
+// hearts[0].replaceWith(circles[0]);
+// wrapper.replaceChild(circles[0], hearts[0]);
 
-console.log(tranfserWaitors(restorantData));
+div.innerHTML = "<h1>hello</h1>";
 
-console.log(restorantData);
+// div.textContent = "Hello";
+
+div.insertAdjacentHTML("afterend", '<h2>hello</h2>');
