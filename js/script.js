eaa2d4c523fbade43ecab905b4d27dc9a0543887
Vsevolod Railean <sevarailean61@gmail.com>
2022-08-21 21:04:29 +0300

    food site (tabs, timer)

--- js/script.js
+++ js/script.js
@@ -1,40 +1,94 @@
-'use strict';
+window.addEventListener('DOMContentLoaded', () => {
 
-const btns = document.querySelectorAll('button'),
-	  wrapper = document.querySelector('.btn-block');
+	//Tabs
+	const tabs =  document.querySelectorAll('.tabheader__item'),
+		  tabsContent = document.querySelectorAll('.tabcontent'),
+		  tabsParent = document.querySelector('.tabheader__items');
 
+	function hideTabContent() {
+		tabsContent.forEach(item =>{
+			item.classList.add('hide');
+			item.classList.remove('show', 'fade');
+		});
 
-// console.log(btns[0].classList.length);
-// console.log(btns[0].classList.item(0));
-// console.log(btns[1].classList.add('red'));
-// console.log(btns[0].classList.remove('blue'));
-// console.log(btns[0].classList.toggle('blue'));
-
-
-// if (btns[1].classList.contains('red')) {
-// 	console.log('red');
-// }
-
-btns[0].addEventListener('click', () => {
-	if (!btns[1].classList.contains('red')) {
-		btns[1].classList.add('red');
-	} else {
-		btns[1].classList.remove('red');
+		tabs.forEach(item => {
+			item.classList.remove('tabheader__item_active');
+		});
 	}
+
+	function showTabContent(i = 0) {
+		tabsContent[i].classList.add('show', 'fade');
+		tabsContent[i].classList.remove('hide');
+		tabs[i].classList.add('tabheader__item_active');
+	}
+
+	hideTabContent();
+	showTabContent();
+
+	tabsParent.addEventListener('click', (event) => {
+		const target = event.target;
+
+		if (target && target.classList.contains('tabheader__item')) {
+			tabs.forEach((item, i) => {
+				if (target == item) {
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
+		}
+	});
+
+	//Timer
+	const deadLine = '2022-08-28';
+
+	function getTimeRemaining(endtime) {
+		const t = Date.parse(endtime) - Date.parse(new Date()),
+			  days = Math.floor(t / (1000 * 60 * 60 * 24)),
+			  hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+			  minutes = Math.floor((t / 1000 / 60) % 60),
+			  seconds = Math.floor((t / 1000) % 60);
+
+		return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
+
+	function getZero(num) {
+		if (num >= 0 && num < 10) {
+			return `0${num}`;
+		} else {
+			return num;
+		}
+	}
+
+	function setClock(selector, endtime) {
+		const timer = document.querySelector(selector),
+			  days = timer.querySelector('#days'),
+			  hours = timer.querySelector('#hours'),
+			  minutes = timer.querySelector('#minutes'),
+			  seconds = timer.querySelector('#seconds'),
+			  timeInterval = setInterval(updateClock, 1000);
+
+		updateClock();
+		
+		function updateClock() {
+			const t = getTimeRemaining(endtime);
+
+			days.innerHTML = getZero(t.days);
+			hours.innerHTML = getZero(t.hours);
+			minutes.innerHTML = getZero(t.minutes);
+			seconds.innerHTML = getZero(t.seconds);
+
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+	}
+
+	setClock('.timer', deadLine);
 });
 
-wrapper.addEventListener('click', (event) => {
-	if (event.target && event.target.matches("button.red")) {
-		console.log("hello");
-	}
-});
-
-// btns.forEach(btn => {
-// 	btn.addEventListener('click', () => {
-// 		console.log("Hello");
-// 	});
-// });
-
-const btn = document.createElement('button');
-btn.classList.add('red');
-wrapper.append(btn);
